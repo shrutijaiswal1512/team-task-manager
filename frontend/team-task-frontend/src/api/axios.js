12e8a63801226aa21f15ec1283c74c6f0daed61d
shrutijaiswal1512 <jaiswalshruti412@gmail.com>
2026-05-04 13:41:56 +0530

    frontend ready for deploy

--- frontend/team-task-frontend/src/api/axios.js
+++ frontend/team-task-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-vmga.onrender.com",
+  baseURL:  import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
